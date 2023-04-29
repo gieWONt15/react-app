238cfb6a5291e4d46f5bfbbed35eef9998fc8d99
gieWONt15 <pawdyslaw@gmail.com>
2023-04-30 00:42:39 +0200

    Create first custom component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import ExpanseItem from "./components/ExpenseItem";
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <ExpanseItem></ExpanseItem>
     </div>
   );
 }
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,5 @@
+function ExpanseItem() {
+  return <h2>Expanse item!</h2>
+}
+
+export default ExpanseItem
