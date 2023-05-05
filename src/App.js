02eeda9e413d6cf3068813bc3a8bed9f90e724f4
gieWONt15 <pawdyslaw@gmail.com>
2023-05-05 12:40:13 +0200

    Samodzielne zadanie

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpanseItem from "./components/ExpenseItem";
+import Expenses from './components/Expenses'
 
 function App() {
   const expenses = [
@@ -24,29 +24,7 @@ function App() {
   ];
 
   return (
-    <div>
-      <h2>Let's get started!</h2>
-      <ExpanseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpanseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpanseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpanseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
-    </div>
+    <Expenses items={expenses} />
   );
 }
 
